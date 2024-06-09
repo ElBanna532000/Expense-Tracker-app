3eaa54917c8cdccf3188fd3f5fa08571eb37e522
mahmoud <mahmoudelbanna136@gmail.com>
2024-06-09 13:23:09 +0300

    initial commit

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+        // Properties corresponding to model classes 
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
